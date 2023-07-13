c59f694f9ecd78acc5a1cfe7480b387bb233bcb1
PAUL MCCORMICK <COMICALS069@ICLOUD.COM>
2023-07-13 19:50:49 +0100

    Initial Commit

---
+++ BOOKAMOVEUK/BOOKAMOVEUKApp.swift
@@ -0,0 +1,17 @@
+//
+//  BOOKAMOVEUKApp.swift
+//  BOOKAMOVEUK
+//
+//  Created by Paul Mccormick on 13/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct BOOKAMOVEUKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
